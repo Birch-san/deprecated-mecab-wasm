0de3133ed957ce29416a71e90789d8fe8a441033
Alex Birch <vengeance.mx@gmail.com>
2016-03-20 18:16:39 +0000

    remove unused code from html, enable compression on Node server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var compression = require('compression');
 
+app.use(compression({ threshold: 0 }));
 app.use('/', express.static('demo'));
 
 app.listen(3000, function () {
